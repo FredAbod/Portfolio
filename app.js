838654a5ef4b27331d826dca706723a170e20cf4
unknown <fredrickbolutife@gmail.com>
2024-03-19 15:31:36 +0100

    first commit

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 import portfolioRouter from '../src/resources/user/routes/user.routes.js'
 import articlesRouter from '../src/resources/user/routes/articles.routes.js'
 import dotenv from "dotenv"
+import cors from "cors"
 
 
 dotenv.config();
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
